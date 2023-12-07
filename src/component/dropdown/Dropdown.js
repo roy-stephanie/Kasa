7976488595e66908cc0fd89b290cc871c4f2ff92
roy-stephanie <korettex@gmail.com>
2023-12-07 13:51:54 +0100

    Refactoring Transition Dropdown

--- src/component/dropdown/Dropdown.js
+++ src/component/dropdown/Dropdown.js
@@ -1,9 +1,17 @@
 import './Dropdown.scss';
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import arrow_up from './img/arrow_up.png';
 
 export default function Dropdown({ status, listing, title, children }) {
   const [open, setOpen] = useState(status);
+  const contentRef = useRef(null);
+  const contentStatus = useRef(null);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      contentStatus.current.style.maxHeight = open ? `${contentRef.current.scrollHeight + 50}px` : '0';
+    }
+  }, [open]);
 
   return (
     <div className="Dropdown">
@@ -11,14 +19,16 @@ export default function Dropdown({ status, listing, title, children }) {
         <span>{title}</span>
         <img className={open ? "Dropdown_Title_Close_Active" : "Dropdown_Title_Close"} src={arrow_up} alt="" />
       </div>
-      <div className={open ? "Dropdown_Content Active" : "Dropdown_Content InActive"}>
-        {!listing && children}
-        {listing &&
-          <ul>
-          {children.map((item, index) => {
-            return <li key={`${index}-${Math.random()}`}>{item}</li>;
-          })}
-        </ul>}
+      <div className={"Dropdown_Content"} ref={contentRef}>
+        <div className={`Dropdown_Content_${open ? "Active" : "Disable"}`} ref={contentStatus}>
+          {!listing && <p>{children}</p>}
+          {listing &&
+            <ul>
+              {children.map((item, index) => {
+                return <li key={`${index}-${Math.random()}`}>{item}</li>;
+              })}
+            </ul>}
+        </div>
       </div>
     </div>
   );
